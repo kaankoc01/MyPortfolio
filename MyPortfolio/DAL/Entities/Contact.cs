81520edcad44bfbaa2b9f55dfa6313a7684cc842
kaan koc <kaankoc01@gmail.com>
2024-06-16 03:33:56 +0300

    Udemy Eğitimi Son Hali

---
+++ MyPortfolio/DAL/Entities/Contact.cs
@@ -0,0 +1,15 @@
+﻿namespace MyPortfolio.DAL.Entities
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string phone1 { get; set; }
+        public string phone2 { get; set; }
+        public string email1 { get; set; }
+        public string email2 { get; set; }
+        public string Address { get; set; }
+
+    }
+}
---
+++ MyPortfolio/Migrations/20240615224201_mig2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPortfolio.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Abouts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SubDescription",
+                table: "Abouts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Details",
+                table: "Abouts",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateTable(
+                name: "ToDoLists",
+                columns: table => new
+                {
+                    ToDoListId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoLists", x => x.ToDoListId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoLists");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Title",
+                table: "Abouts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SubDescription",
+                table: "Abouts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Details",
+                table: "Abouts",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
